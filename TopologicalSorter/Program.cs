6c8e8b45f872c0423a85ddd63e8874f63188e33d
Andrew-Seagle <andrewseagle93@gmail.com>
2021-07-14 18:19:45 -0400

    Built skeleton of components needed for sorter.

--- TopologicalSorter/Program.cs
+++ TopologicalSorter/Program.cs
@@ -1,12 +1,9 @@
-﻿using System;
-
-namespace TopologicalSorter
+﻿namespace TopologicalSorter
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
